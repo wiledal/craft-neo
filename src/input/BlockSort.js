3cfafcabd17e2fa04617e083a3e99ac6d80e459b
Benjamin Fleming <benjfleming21@gmail.com>
2016-06-25 20:25:51 +1000

    Blocks now correctly drag to the correct positions

--- src/input/BlockSort.js
+++ src/input/BlockSort.js
@@ -1,9 +1,13 @@
 import $ from 'jquery'
 import Garnish from 'garnish'
 
-const DragSort = Garnish.Drag.extend({
+const debug = false
+
+const BlockSort = Garnish.Drag.extend({
 
 	blocks: null,
+
+	_draggeeBlock: null,
 
 	init(items, settings)
 	{
@@ -13,44 +17,40 @@ const DragSort = Garnish.Drag.extend({
 			items = null
 		}
 
-		settings = $.extend({}, DragSort.defaults, settings)
+		settings = $.extend({}, BlockSort.defaults, settings)
+		settings.axis = Garnish.Y_AXIS
+
 		this.base(items, settings)
 
 		this.blocks = []
 
-		const $marker = $('<div>').css({
-			position: 'absolute',
-			zIndex: 9999,
-			left: 0,
-			right: 0,
-			borderTop: '1px solid red',
-			textAlign: 'right',
-			fontFamily: 'monospace'
-		})
-
-		Garnish.$bod.append($marker)
-		Garnish.$bod.on('mousemove', e =>
+		if(debug)
 		{
-			this.mouseX = e.pageX
-			this.mouseY = e.pageY
+			const $marker = $('<div>').css({
+				position: 'absolute',
+				zIndex: 9999,
+				left: 0,
+				right: 0,
+				borderTop: '1px solid red',
+				textAlign: 'right',
+				fontFamily: 'monospace'
+			})
 
-			const midpoint = this._getClosestMidpoint()
-			$marker.css('top', midpoint.position + 'px')
-			$marker.text(midpoint.type + ' - ' + midpoint.block.getId() + ' (' + midpoint.block.getBlockType().getHandle() + ')')
-		})
+			Garnish.$bod.append($marker)
+			Garnish.$bod.on('mousemove', e =>
+			{
+				this.mouseY = e.pageY
+
+				const midpoint = this._getClosestMidpoint()
+				$marker.css('top', midpoint.position + 'px')
+				$marker.text(midpoint.type + ' - ' + midpoint.block.getId() + ' (' + midpoint.block.getBlockType().getHandle() + ')')
+			})
+		}
 	},
 
 	getHelperTargetX()
 	{
-		const magnet = this.settings.magnetStrength
-
-		if(magnet != 1)
-		{
-			const draggeeOffsetX = this.$draggee.offset().left
-			return draggeeOffsetX + ((this.mouseX - this.mouseOffsetX - draggeeOffsetX) / magnet)
-		}
-
-		return this.base()
+		return this.$draggee.offset().left
 	},
 
 	getHelperTargetY()
@@ -66,13 +66,27 @@ const DragSort = Garnish.Drag.extend({
 		return this.base()
 	},
 
+	getBlockByElement($block)
+	{
+		return this.blocks.find(block => block.$container.is($block))
+	},
+
 	onDragStart()
 	{
+		this._draggeeBlock = this.getBlockByElement(this.$draggee[0])
+
 		this.base()
 	},
 
 	onDrag()
 	{
+		const midpoint = this._getClosestMidpoint()
+
+		if(midpoint.block !== this._draggeeBlock)
+		{
+			this._moveDraggeeToBlock(midpoint.block, midpoint.type);
+		}
+
 		this.base()
 	},
 
@@ -101,24 +115,27 @@ const DragSort = Garnish.Drag.extend({
 			block: null,
 			position: 0,
 			distance: Number.MAX_VALUE,
-			type: 'content'
+			type: BlockSort.TYPE_CONTENT
 		}
 
 		for(let block of this.blocks)
 		{
-			const midpoints = this._getBlockMidpoints(block)
-
-			for(let type of Object.keys(midpoints))
+			if(!this.$draggee.is(block.$container) || block === this._draggeeBlock)
 			{
-				const midpoint = midpoints[type]
-				const distance = Math.abs(this.mouseY - midpoint)
+				const midpoints = this._getBlockMidpoints(block)
 
-				if(distance < closest.distance)
+				for(let type of Object.keys(midpoints))
 				{
-					closest.block = block
-					closest.position = midpoint
-					closest.distance = distance
-					closest.type = type
+					const midpoint = midpoints[type]
+					const distance = Math.abs(this.mouseY - midpoint)
+
+					if(distance < closest.distance)
+					{
+						closest.block = block
+						closest.position = midpoint
+						closest.distance = distance
+						closest.type = type
+					}
 				}
 			}
 		}
@@ -141,20 +158,50 @@ const DragSort = Garnish.Drag.extend({
 		const contentHeight = isExpanded ? block.$contentContainer.height() : 0
 		const childrenHeight = isExpanded ? block.$childrenContainer.height() : 0
 
-		const midpoints = {
-			content: offset + (topbarHeight + contentHeight) / 2
-		}
+		const midpoints = {}
+		midpoints[BlockSort.TYPE_CONTENT] = offset + (topbarHeight + contentHeight) / 2
 
 		if(childrenHeight > 0 && block.isExpanded())
 		{
 			const buttonsHeight = block.getButtons().$container.height()
-			midpoints.children = offset + blockHeight - border - (padding + buttonsHeight + margin) / 2
+			midpoints[BlockSort.TYPE_CHILDREN] = offset + blockHeight - border - (padding + buttonsHeight + margin) / 2
 		}
 
 		return midpoints
+	},
+
+	_moveDraggeeToBlock: function(block, type = BlockSort.TYPE_CONTENT)
+	{
+		if(type === BlockSort.TYPE_CHILDREN)
+		{
+			block.$blocksContainer.append(this.$draggee)
+		}
+		else
+		{
+			block.$container.before(this.$draggee)
+		}
+
+		this._updateHelperAppearance()
+	},
+
+	_updateHelperAppearance()
+	{
+		for(let $helper of this.helpers)
+		{
+			const id = $helper.data('neo-b-id')
+			const block = this.blocks.find(b => b.$container.data('neo-b-id') == id)
+
+			$helper.css({
+				width: block.$container.width() + 1,
+				height: block.$container.height()
+			})
+		}
 	}
 
 }, {
+
+	TYPE_CONTENT: 'content',
+	TYPE_CHILDREN: 'children',
 
 	defaults: {
 		container: null,
@@ -165,4 +212,4 @@ const DragSort = Garnish.Drag.extend({
 	}
 })
 
-export default DragSort
+export default BlockSort
